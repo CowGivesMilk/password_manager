bdaa606bd8c68e175c5fa1ccd2698562ca3f5d81
CowGivesMilk <nimeshpoudel89@gmail.com>
2025-08-19 20:09:51 +0545

    Added place for future error handling

--- include/enc_dec.hpp
+++ include/enc_dec.hpp
@@ -9,16 +9,16 @@ class EncDec {
  public:
   static std::pair<std::array<CryptoPP::byte, 32>,
                    std::array<CryptoPP::byte, 16>>
-  generate_key_salt(const std::string &password);
+  generate_key_salt(const std::string &password) noexcept;
 
   static std::array<CryptoPP::byte, 32> generate_key_from_salt(
       const std::string &password, const std::array<CryptoPP::byte, 16> &salt);
-  static std::array<CryptoPP::byte, 12> generate_nonce();
+  static std::array<CryptoPP::byte, 12> generate_nonce() noexcept;
 
-  static bool encrypt(const std::string &file_path,
+  static void encrypt(const std::string &file_path,
                       const std::array<CryptoPP::byte, 32> &key,
                       const std::array<CryptoPP::byte, 12> &nonce);
-  static std::optional<std::string> decrypt(
-      const std::string &file_path, const std::array<CryptoPP::byte, 32> &key);
+  static std::string decrypt(const std::string &file_path,
+                             const std::array<CryptoPP::byte, 32> &key);
 };
 #endif  // ENC_DEC_H
--- src/enc_dec.cpp
+++ src/enc_dec.cpp
@@ -17,11 +17,11 @@
 #include <string>
 
 #include "file_handler.hpp"
-const size_t ITERATIONS = 100000;
+const size_t ITERATIONS = 100'000;
 const CryptoPP::byte PURPOSE = 0;  // No purpose
 
 std::pair<std::array<CryptoPP::byte, 32>, std::array<CryptoPP::byte, 16>>
-EncDec::generate_key_salt(const std::string &password) {
+EncDec::generate_key_salt(const std::string &password) noexcept {
   std::pair<std::array<CryptoPP::byte, 32>, std::array<CryptoPP::byte, 16>>
       result;
 
@@ -41,7 +41,8 @@ EncDec::generate_key_salt(const std::string &password) {
 }
 
 std::array<CryptoPP::byte, 32> EncDec::generate_key_from_salt(
-    const std::string &password, const std::array<CryptoPP::byte, 16> &salt) {
+    const std::string &password,
+    const std::array<CryptoPP::byte, 16> &salt) noexcept {
   std::array<CryptoPP::byte, 32> key;
   CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA256> pbkdf;
   pbkdf.DeriveKey(key.data(), key.size(), PURPOSE,
@@ -56,62 +57,79 @@ std::array<CryptoPP::byte, 12> EncDec::generate_nonce() {
   prng.GenerateBlock(nonce.data(), nonce.size());
   return nonce;
 }
-bool EncDec::encrypt(const std::string &file_path,
+void EncDec::encrypt(const std::string &file_path,
                      const std::array<CryptoPP::byte, 32> &key,
                      const std::array<CryptoPP::byte, 12> &nonce) {
   std::string plain_text = FileHandler::read_file(file_path);
   if (plain_text.empty()) {
-    std::cerr << "Empty file entered for input\n";
-    return false;
+    // std::cerr << "Empty file entered for input\n";
+    throw std::invalid_argument("Empty file to encrypt");
   }
-  std::string cipher;
-  CryptoPP::GCM<CryptoPP::AES>::Encryption gcm_aes;
-  gcm_aes.SetKeyWithIV(key.data(), key.size(), nonce.data(), nonce.size());
+  try {
+    std::string cipher;
+    CryptoPP::GCM<CryptoPP::AES>::Encryption gcm_aes;
+    gcm_aes.SetKeyWithIV(key.data(), key.size(), nonce.data(), nonce.size());
 
-  CryptoPP::AuthenticatedEncryptionFilter encryption_filter(
-      gcm_aes, new CryptoPP::StringSink(cipher), false, 16);
-  encryption_filter.Put(
-      reinterpret_cast<const CryptoPP::byte *>(plain_text.data()),
-      plain_text.size());
-  encryption_filter.MessageEnd();
-  std::ofstream encrypted_file(file_path + ".enc", std::ios::binary);
-  encrypted_file.write(reinterpret_cast<const char *>(nonce.data()),
-                       nonce.size());
-  encrypted_file.write(cipher.data(), cipher.size());
-  return encrypted_file.good();
+    CryptoPP::AuthenticatedEncryptionFilter encryption_filter(
+        gcm_aes, new CryptoPP::StringSink(cipher), false, 16);
+    encryption_filter.Put(
+        reinterpret_cast<const CryptoPP::byte *>(plain_text.data()),
+        plain_text.size());
+    encryption_filter.MessageEnd();
+    std::ofstream encrypted_file(file_path + ".enc", std::ios::binary);
+    encrypted_file.write(reinterpret_cast<const char *>(nonce.data()),
+                         nonce.size());
+    encrypted_file.write(cipher.data(), cipher.size());
+  } catch (CryptoPP::BufferedTransformation::NoChannelSupport &e) {
+    std::cerr << "Caught NoChannelSupport " << e.what() << std::endl;
+  } catch (CryptoPP::AuthenticatedSymmetricCipher::BadState &e) {
+    std::cerr << "Caught BadState " << e.what() << std::endl;
+  } catch (CryptoPP::InvalidArgument &e) {
+    std::cerr << "Caught InvalidArgument " << e.what() << std::endl;
+  } catch (...) {
+    std::cerr << "Unexpected error occured\n";
+  }
 }
 
-std::optional<std::string> EncDec::decrypt(
-    const std::string &encrypted_file_path,
-    const std::array<CryptoPP::byte, 32> &key) {
+std::string EncDec::decrypt(const std::string &encrypted_file_path,
+                            const std::array<CryptoPP::byte, 32> &key) {
   std::string cipher = FileHandler::read_file(encrypted_file_path);
   if (cipher.size() <= 12 + 16) {  // Check nonce + min ciphertext + MAC
-    std::cerr << "Invalid file size\n";
-    return std::nullopt;
+    throw std::runtime_error(
+        "File is too small to contain [IV cipher_text MAC]");
   }
+  try {
+    // Extract components
+    std::array<CryptoPP::byte, 12> nonce;
+    memcpy(nonce.data(), cipher.data(), 12);
+    std::string enc = cipher.substr(12, cipher.size() - 12 - 16);
+    std::string mac = cipher.substr(cipher.size() - 16);
 
-  // Extract components
-  std::array<CryptoPP::byte, 12> nonce;
-  memcpy(nonce.data(), cipher.data(), 12);
-  std::string enc = cipher.substr(12, cipher.size() - 12 - 16);
-  std::string mac = cipher.substr(cipher.size() - 16);
+    // Decrypt
+    CryptoPP::GCM<CryptoPP::AES>::Decryption aes_gcm;
+    aes_gcm.SetKeyWithIV(key.data(), key.size(), nonce.data(), nonce.size());
 
-  // Decrypt
-  CryptoPP::GCM<CryptoPP::AES>::Decryption aes_gcm;
-  aes_gcm.SetKeyWithIV(key.data(), key.size(), nonce.data(), nonce.size());
+    std::string recovered;
+    CryptoPP::AuthenticatedDecryptionFilter decryption_filter(
+        aes_gcm,
+        new CryptoPP::StringSink(recovered),  // Critical: sink for plaintext
+        CryptoPP::AuthenticatedDecryptionFilter::MAC_AT_BEGIN |
+            CryptoPP::AuthenticatedDecryptionFilter::THROW_EXCEPTION,
+        16);
 
-  std::string recovered;
-  CryptoPP::AuthenticatedDecryptionFilter decryption_filter(
-      aes_gcm,
-      new CryptoPP::StringSink(recovered),  // Critical: sink for plaintext
-      CryptoPP::AuthenticatedDecryptionFilter::MAC_AT_BEGIN |
-          CryptoPP::AuthenticatedDecryptionFilter::THROW_EXCEPTION,
-      16);
+    // Feed MAC first then ciphertext
+    decryption_filter.Put((const CryptoPP::byte *)mac.data(), mac.size());
+    decryption_filter.Put((const CryptoPP::byte *)enc.data(), enc.size());
+    decryption_filter.MessageEnd();
 
-  // Feed MAC first then ciphertext
-  decryption_filter.Put((const CryptoPP::byte *)mac.data(), mac.size());
-  decryption_filter.Put((const CryptoPP::byte *)enc.data(), enc.size());
-  decryption_filter.MessageEnd();
-
-  return recovered;
+    return recovered;
+  } catch (CryptoPP::InvalidArgument &e) {
+    std::cerr << "Caught InvalidArgument " << e.what() << std::endl;
+  } catch (CryptoPP::AuthenticatedSymmetricCipher::BadState &e) {
+    std::cerr << "Caught BadState " << e.what() << std::endl;
+  } catch (CryptoPP::HashVerificationFilter::HashVerificationFailed &e) {
+    std::cerr << "Caught HashVerificationFailed " << e.what() << std::endl;
+  } catch (...) {
+    std::cerr << "Unexpected Exception thrown\n";
+  }
 }
